127cc876b6922ccc0b9b5c4dc7e19b068779178b
herrhu <herrhu@tencent.com>
2023-01-08 16:59:02 +0800

    first commit

---
+++ main.go
@@ -0,0 +1,127 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"net/http"
+	"os"
+	"sync/atomic"
+	"time"
+
+	"github.com/hashicorp/raft"
+
+	fsm "github.com/herrhu97/raft-demo/memfsm"
+	myraft "github.com/herrhu97/raft-demo/raftwrapper"
+)
+
+var (
+	httpAddr    string
+	raftAddr    string
+	raftId      string
+	raftCluster string
+	raftDir     string
+)
+
+var (
+	isLeader int64
+)
+
+func init() {
+	flag.StringVar(&httpAddr, "http_addr", "127.0.0.1:7001", "http listen addr")
+	flag.StringVar(&raftAddr, "raft_addr", "127.0.0.1:7000", "raft listen addr")
+	flag.StringVar(&raftId, "raft_id", "1", "raft id")
+	flag.StringVar(&raftCluster, "raft_cluster", "1/127.0.0.1:7000,2/127.0.0.1:8000,3/127.0.0.1:9000", "cluster info")
+}
+
+func main() {
+	flag.Parse()
+	// 初始化配置
+	if httpAddr == "" || raftAddr == "" || raftId == "" || raftCluster == "" {
+		fmt.Println("config error")
+		os.Exit(1)
+		return
+	}
+	raftDir := "node/raft_" + raftId
+	os.MkdirAll(raftDir, 0700)
+
+	// 初始化raft
+	myRaft, fm, err := myraft.NewRaftWrapper(raftAddr, raftId, raftDir)
+	if err != nil {
+		fmt.Println("NewMyRaft error ", err)
+		os.Exit(1)
+		return
+	}
+
+	// 启动raft
+	myraft.Bootstrap(myRaft, raftId, raftAddr, raftCluster)
+
+	// 监听leader变化（使用此方法无法保证强一致性读，仅做leader变化过程观察）
+	go func() {
+		for leader := range myRaft.LeaderCh() {
+			if leader {
+				atomic.StoreInt64(&isLeader, 1)
+			} else {
+				atomic.StoreInt64(&isLeader, 0)
+			}
+		}
+	}()
+
+	// 启动http server
+	httpServer := HttpServer{
+		ctx: myRaft,
+		fsm: fm,
+	}
+
+	http.HandleFunc("/set", httpServer.Set)
+	http.HandleFunc("/get", httpServer.Get)
+	http.ListenAndServe(httpAddr, nil)
+
+	// 关闭raft
+	shutdownFuture := myRaft.Shutdown()
+	if err := shutdownFuture.Error(); err != nil {
+		fmt.Printf("shutdown raft error:%v \n", err)
+	}
+
+	// 退出http server
+	fmt.Println("shutdown kv http server")
+}
+
+type HttpServer struct {
+	ctx *raft.Raft
+	fsm *fsm.MemFsm
+}
+
+func (h HttpServer) Set(w http.ResponseWriter, r *http.Request) {
+	if atomic.LoadInt64(&isLeader) == 0 {
+		fmt.Fprintf(w, "not leader")
+		return
+	}
+	vars := r.URL.Query()
+	key := vars.Get("key")
+	value := vars.Get("value")
+	if key == "" || value == "" {
+		fmt.Fprintf(w, "error key or value")
+		return
+	}
+
+	data := "set" + "," + key + "," + value
+	future := h.ctx.Apply([]byte(data), 5*time.Second)
+	if err := future.Error(); err != nil {
+		fmt.Fprintf(w, "error:"+err.Error())
+		return
+	}
+	fmt.Fprintf(w, "ok")
+	return
+}
+
+func (h HttpServer) Get(w http.ResponseWriter, r *http.Request) {
+	vars := r.URL.Query()
+	key := vars.Get("key")
+	if key == "" {
+		fmt.Fprintf(w, "error key")
+		return
+	}
+	value := h.fsm.DataBase.Get(key)
+	fmt.Fprintf(w, value)
+	return
+}
---
+++ memfsm/memfsm.go
@@ -0,0 +1,81 @@
+package fsm
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"strings"
+	"sync"
+
+	"github.com/hashicorp/raft"
+)
+
+type MemFsm struct {
+	DataBase database
+}
+
+func NewMemFsm() *MemFsm {
+	fsm := &MemFsm{
+		DataBase: NewDatabase(),
+	}
+	return fsm
+}
+
+func (f *MemFsm) Apply(l *raft.Log) interface{} {
+	fmt.Println("apply data:", string(l.Data))
+	data := strings.Split(string(l.Data), ",")
+	op := data[0]
+	if op == "set" {
+		key := data[1]
+		value := data[2]
+		f.DataBase.Set(key, value)
+	}
+
+	return nil
+}
+
+func (f *MemFsm) Snapshot() (raft.FSMSnapshot, error) {
+	return &f.DataBase, nil
+}
+
+func (f *MemFsm) Restore(io.ReadCloser) error {
+	return nil
+}
+
+type database struct {
+	Data map[string]string
+	mu   sync.Mutex
+}
+
+func NewDatabase() database {
+	return database{
+		Data: make(map[string]string),
+	}
+}
+
+func (d *database) Get(key string) string {
+	d.mu.Lock()
+	value := d.Data[key]
+	d.mu.Unlock()
+	return value
+}
+
+func (d *database) Set(key, value string) {
+	d.mu.Lock()
+	d.Data[key] = value
+	d.mu.Unlock()
+}
+
+func (d *database) Persist(sink raft.SnapshotSink) error {
+	d.mu.Lock()
+	data, err := json.Marshal(d.Data)
+	d.mu.Unlock()
+	if err != nil {
+		return err
+	}
+	sink.Write(data)
+	sink.Close()
+	return nil
+}
+
+func (d *database) Release() {}
